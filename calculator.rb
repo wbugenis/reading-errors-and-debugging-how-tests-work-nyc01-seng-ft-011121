99eb9aa33945e1b3d98b8aa765ef20f3e9f008f1
William Bugenis <wbugenis@gmail.com>
2020-12-03 23:37:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 5
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+dividend = first_number / second_number
