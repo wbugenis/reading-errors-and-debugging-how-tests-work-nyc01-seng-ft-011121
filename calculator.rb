346f13907979a1cbe6b2f108314de73a7ae338b0
William Bugenis <wbugenis@gmail.com>
2020-12-03 23:38:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-dividend = first_number / second_number
+quotient = first_number / second_number
